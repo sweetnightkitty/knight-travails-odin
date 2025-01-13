c8eccb0d1f0ea4f2b816e44145209215463b25f5
SaraToth <saratoth101@gmail.com>
2025-01-13 18:45:29 +0900

    Create the chessboard as an empty 8x8 adjacency list

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+const boardSize = 8;
+
+const board = Array.from({length: boardSize}, ()=> []);
+console.log(board);
+
 function addEdge(adjList, i, j) {
     adjList[i].push(j);
     adjList[j].push(i); //Knights move in an undirected pattern
 }
+
