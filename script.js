4b2ceffe7e0b456c7752a761053f2ef0cf20db4b
SaraToth <saratoth101@gmail.com>
2025-01-14 08:58:21 +0900

    Add recursion to calculate further potential moves, and refactor addEdge function to prevent duplicating any pre existing moves

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ const board = Array.from({length: boardSize}, ()=> []);
 //Adds an edge to the gameboard
 function addEdge(adjList, i, j) {
 
+    //if row i exists in the list; if j exists return to avoid duplicates
+    if(adjList[i]) {
+        for(let n = 0; n < adjList[i].length; n++) {
+            if(adjList[i][n] == j) {
+                return;
+            }
+        } 
+    }
+
     //i and j below 0 or higher than 7 doesn't exist
     if((i > -1 && i < 8) && (j > -1 && j < 8)) {
         adjList[i].push(j);
@@ -21,8 +30,20 @@ function addEdge(adjList, i, j) {
 function createPossibleMoves(start) {
     let x = start[0];
     let y = start[1];
+
+    if(x < 0 || y < 0 || x > 7 || y > 7) {
+        return;
+    }
+
+    console.log("test:");
+    console.log(JSON.parse(JSON.stringify(board[x])));
+
+    console.log(board);
+
     addEdge(board, x, y);
     addEdge(board, (x - 1), (y + 2));
+    //Recursively check possible moves
+    createPossibleMoves([(x - 2), (y - 1)]);
     addEdge(board, (x - 2), (y - 1));
     addEdge(board, (x - 2), (y + 1));
     addEdge(board, (x + 1), (y + 2));
