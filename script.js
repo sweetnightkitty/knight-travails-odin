be0fddd29e2b0e08412e2a3e68b4b2fff3dc7656
SaraToth <saratoth101@gmail.com>
2025-01-14 07:36:24 +0900

    Add condition to function addEdge to ensure that no edges are created for a place in the board that doesn't exist

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const board = Array.from({length: boardSize}, ()=> []);
 console.log(board);
 
 function addEdge(adjList, i, j) {
-    adjList[i].push(j);
-    adjList[j].push(i); //Knights move in an undirected pattern
+    if((i > -1 && i < 8) && (j > -1 && j < 8)) {
+        adjList[i].push(j);
+        adjList[j].push(i); //Knights move in an undirected pattern
+    }
 }
+
 
