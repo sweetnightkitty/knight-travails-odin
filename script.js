d2377f9b8553252e2cfcb4f3f8fc089890c7b525
SaraToth <saratoth101@gmail.com>
2025-01-14 07:45:36 +0900

    Write function that will take the start point and generate all possible moves into the board adjacency list, and update addEdge function to prevent duplicates

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const boardSize = 8;
 
 const board = Array.from({length: boardSize}, ()=> []);
-console.log(board);
 
 function addEdge(adjList, i, j) {
+
+    //i and j below 0 or higher than 7 doesn't exist
     if((i > -1 && i < 8) && (j > -1 && j < 8)) {
         adjList[i].push(j);
-        adjList[j].push(i); //Knights move in an undirected pattern
+        
+        //if i = j there would otherwise be a duplicate
+        if(i != j) {
+            adjList[j].push(i); //Knights move in an undirected pattern
+        }
     }
 }
 
+//start must be a coordinate [0, 1]
+function createPossibleMoves(start) {
+    let x = start[0];
+    let y = start[1];
+    addEdge(board, x, y);
+    addEdge(board, (x - 1), (y + 2));
+    addEdge(board, (x - 2), (y - 1));
+    addEdge(board, (x - 2), (y + 1));
+    addEdge(board, (x + 1), (y + 2));
+}
+
+createPossibleMoves([3, 3]);
+console.log(board);
 
