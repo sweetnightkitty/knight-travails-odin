4771e445f822740471c3ec40f0576bce501ddb63
SaraToth <saratoth101@gmail.com>
2025-01-14 07:49:11 +0900

    Add comments for better readability and understanding

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const boardSize = 8;
 
+//Create an empty 8 x 8 array
 const board = Array.from({length: boardSize}, ()=> []);
 
+//Adds an edge to the gameboard
 function addEdge(adjList, i, j) {
 
     //i and j below 0 or higher than 7 doesn't exist
     if((i > -1 && i < 8) && (j > -1 && j < 8)) {
         adjList[i].push(j);
-        
+
         //if i = j there would otherwise be a duplicate
         if(i != j) {
             adjList[j].push(i); //Knights move in an undirected pattern
@@ -15,7 +17,7 @@ function addEdge(adjList, i, j) {
     }
 }
 
-//start must be a coordinate [0, 1]
+//Assigns the starting location, and all possible next moves to the board (adjacency list)
 function createPossibleMoves(start) {
     let x = start[0];
     let y = start[1];
@@ -26,6 +28,7 @@ function createPossibleMoves(start) {
     addEdge(board, (x + 1), (y + 2));
 }
 
+//If knight begins on board at 3, 3; board will show all possible moves
 createPossibleMoves([3, 3]);
 console.log(board);
 
