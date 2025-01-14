19b65e34b357ee5325315b9a510bb4c5e7ae6e60
SaraToth <saratoth101@gmail.com>
2025-01-14 17:01:24 +0900

    Write function callback which takes in a parentNode or vertex, and then creates a template for all its next level children which will be built upon to generate all next possible moves

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const boardSize = 8;
 
 const board = Array.from({length: boardSize}, ()=> Array(boardSize));
 
-function newNode(distance = null) {
+function newNode(distance = null, value = null) {
     return {
+        value: value,
         distance: distance,
         predecessor: null
     };
@@ -12,7 +13,7 @@ function newNode(distance = null) {
 const start = [3, 3];
 
 
-function createMoves(start, callback, distance = 0) {
+function createMoves(start, distance = 0) {
     const x = start[0];
     const y = start[1];
 
@@ -22,31 +23,30 @@ function createMoves(start, callback, distance = 0) {
     }
 
     //Store the starting vertex in the graph
-    board[x][y] = newNode(distance);
+    board[x][y] = newNode(distance, start);
     const currentNode = board[x][y];
     const queue = [];
     queue.push(currentNode);
 
     while(queue.length > 0) {
         const node = queue.shift();
-        callback(node); //callback will run all possible moves
+
+        //Returns multiple child items - in an array or object
+        callback(node);
+
+        //
     }
-    //If the vertex has not been visited
-    if(!board[x][y]) {
-
-        //Create new node & enqueue it
-        const currentNode = board[x][y];
-        queue.push(currentNode);
-
-        while(queue.length) {
-
-            //Do stuff with the queue
-        }
-
-
-    //Takes the start and adds it to the graph with 0 distance; stores it as a node
 
 };
+
+//Takes in a parentNode and generates all it's possible next level vertexes
+function callback(parentNode) {
+
+    //Create a generic template for all potential children nodes
+    const childNode = newNode(++parentNode.distance);
+    childNode.predecessor = parentNode;
+
+}
 
 createMoves(start);
 console.log(board);
