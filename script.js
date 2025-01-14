58d2ebcc9f62f9b62254b982b6781e2959872992
SaraToth <saratoth101@gmail.com>
2025-01-14 16:52:15 +0900

    Refactor and simplify createMoves function to add the start position to a queue, and begin a while statement that will add to and process the queue

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function newNode(distance = null) {
 const start = [3, 3];
 
 
-function createMoves(start, distance = 0) {
+function createMoves(start, callback, distance = 0) {
     const x = start[0];
     const y = start[1];
 
@@ -21,9 +21,31 @@ function createMoves(start, distance = 0) {
         return;
     }
 
-    //Takes the start and adds it to the graph with 0 distance; stores it as a node
+    //Store the starting vertex in the graph
     board[x][y] = newNode(distance);
     const currentNode = board[x][y];
+    const queue = [];
+    queue.push(currentNode);
+
+    while(queue.length > 0) {
+        const node = queue.shift();
+        callback(node); //callback will run all possible moves
+    }
+    //If the vertex has not been visited
+    if(!board[x][y]) {
+
+        //Create new node & enqueue it
+        const currentNode = board[x][y];
+        queue.push(currentNode);
+
+        while(queue.length) {
+
+            //Do stuff with the queue
+        }
+
+
+    //Takes the start and adds it to the graph with 0 distance; stores it as a node
+
 };
 
 createMoves(start);
