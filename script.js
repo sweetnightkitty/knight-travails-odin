fa2a0c7a29fcb7d0d414fbdf24762cd08d18f522
SaraToth <saratoth101@gmail.com>
2025-01-14 16:06:20 +0900

    Write function createMoves which takes in a start point and then stores the distance and predecessor of that vertice in the graph

--- script.js
+++ script.js
@@ -9,6 +9,19 @@ function newNode() {
     };
 }
 
+const start = [3, 3];
+
+
+function createMoves(start, distance = 0) {
+    let x = start[0];
+    let y = start[1];
+
+    board[x][y] = newNode();
+    board[x][y].distance = distance;
+};
+
+createMoves(start);
+console.log(board);
 
 
 
